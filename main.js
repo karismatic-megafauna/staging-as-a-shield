f23a181812ed031ce75bd8065f9338e9287c9827
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-10-14 08:47:02 -0700

    Prepare for solution branch

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ import util from './util.js';
 
 x = 3;
 y = 4;
-z = 6;
 
-// call site of our function
+// calculate the area
 area = util.getRectArea(x, y);
 
+// ...
+// ...
+// ...
+
+// calculate the volume
+z = 6;
 volume = area * z;
 
 console.log('area is:', area); // 12
